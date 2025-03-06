b1b7f942dae8f365da2963594b42676b8ddac403
PRAMODH-143 <pramodhp798@gmail.com>
2025-03-06 11:39:52 +0530

    Create  Variables programe.java

---
+++ Variables programe.java
@@ -0,0 +1,13 @@
+// Variables programe in java //
+
+public class Variables
+{
+    public static void main(String[] args)
+    {
+        int id=5;
+        String name="Suneel";
+        double marks = 19.55;
+        System.out.println("id"+" "  +id +" " +"Name"+" "  +name+" " +"Marks"+" " +marks);
+        
+    }
+}
